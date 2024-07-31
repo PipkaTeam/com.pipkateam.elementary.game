351026d211f3ee5db1aae9658326c8c593d2139d
Ikar2014 <vhorily@gmail.com>
2024-07-31 12:50:23 +0300

    Update OnlineManager.cs

--- Runtime/OnlineManager.cs
+++ Runtime/OnlineManager.cs
@@ -4,9 +4,21 @@ namespace Elementary.Game
 {
     public class OnlineManager : Singleton<OnlineManager>
     {
+        public event Action OnRewardedVideoFinishedEvent;
+
         protected override void Start()
         {
             base.Start();
+        }
+
+        protected virtual void ShowRewardedVideo()
+        {
+
+        }
+
+        protected void OnRewardedVideoFinished()
+        {
+            OnRewardedVideoFinishedEvent?.Invoke();
         }
     }
 }
